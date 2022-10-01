ddade5788f035ea5edd3448158574fd436cd1558
kratos0-f <protopovichevgeny0@gmail.com>
2022-10-01 12:48:07 +0300

    Добавлен файл с таблицей, папка со всеми модулями

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
-from tkinter import ttk
-from textEditor import main_loop
+import os
+from modules.textEditor import main_loop
+from modules.moduleTable import Table
 
 window = tk.Tk()
 
@@ -9,6 +10,12 @@ texteditorBtn = tk.Button(
     text="Add new template",
     command=main_loop
 )
+
+filename_tuple = ()
+for filename in os.listdir("data"):
+    filename_tuple += ((filename), )
+file_list = Table(window, headings=("File name"), rows=filename_tuple)
+file_list.pack(expand=tk.YES, fill=tk.BOTH)
 
 texteditorBtn.pack()
 
---
+++ modules/moduleTable.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+class Table(tk.Frame):
+    def __init__(self, parent=None, headings=tuple(), rows=tuple()):
+        super().__init__(parent)
+
+        table = ttk.Treeview(self, show="headings", selectmode="browse")
+        table["columns"] = headings
+        table["displaycolumns"] = headings
+
+        for head in headings:
+            table.heading(head, text=head, anchor=tk.CENTER)
+            table.column(head, anchor=tk.CENTER)
+
+        for row in rows:
+            table.insert('', tk.END, values=tuple(row))
+
+        scrolltable = tk.Scrollbar(self, command=table.yview)
+        table.configure(yscrollcommand=scrolltable.set)
+        scrolltable.pack(side=tk.RIGHT, fill=tk.Y)
+        table.pack(expand=tk.YES, fill=tk.BOTH)
